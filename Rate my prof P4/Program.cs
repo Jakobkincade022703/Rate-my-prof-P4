e2de286921609fdf2b5d5ff3f9b68c3c490dcca0
jakob <jakob@JAKOB>
2024-09-23 10:32:41 -0500

    Add project files.

---
+++ Rate my prof P4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Security;
+
+namespace Rate_my_prof_P4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello!");    
+        }
+    }
+}
